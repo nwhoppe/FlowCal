542ed2072d1b6196905d8fa2327bbcd8aa44accc
nwhoppe <Nick.Hoppe@ucsf.edu>
2019-09-17 11:17:38 -0700

    unfinished commit - just transfering computers

---
+++ dev/plot_expression_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import FlowCal
+import sys
+
+
+def plot_expression_test(input_fcs_files, channel='FITC'):
+    """takes channel, graph labels are from name of each file, one plot"""
+
+
+
+if __name__ == '__main__':
+    input_fcs_files = sys.argv[1:]
+    plot_expression_test(input_fcs_files)
